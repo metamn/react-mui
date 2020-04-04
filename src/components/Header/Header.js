aa6a8eff85dc876b10430a15f02174ba59928c11
metamn <bartus.csongor@gmail.com>
2020-04-04 15:53:58 +0300

    Updating docs

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -25,7 +25,7 @@ const Header = props => {
   return (
     <AppBar className="Header" position="static">
       <Toolbar>
-        <Grid container spacing={1}>
+        <Grid container spacing={1} alignItems="center">
           <Grid item>
             <Logo />
           </Grid>
