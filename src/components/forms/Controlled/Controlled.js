4d440a8dc1b43d6626913a1691434e6496508ea1
metamn <bartus.csongor@gmail.com>
2020-04-07 10:20:15 +0300

    Working on the text field

--- src/components/forms/Controlled/Controlled.js
+++ src/components/forms/Controlled/Controlled.js
@@ -98,7 +98,7 @@ const Controlled = props => {
             </CardContent>
           </Card>
         </Grid>
-        <Grid item xs={12}>
+        <Grid item xs={12} md={6}>
           <TextField />
         </Grid>
       </Grid>
