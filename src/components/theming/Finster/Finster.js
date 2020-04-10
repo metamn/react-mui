fd89e43d8150fbc4f5fee3a7160016457c2baba6
metamn <bartus.csongor@gmail.com>
2020-04-10 08:03:48 +0300

    Working on the custom theme

--- src/components/theming/Finster/Finster.js
+++ src/components/theming/Finster/Finster.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import PropTypes from "prop-types";
 
 import { ThemeContext } from "../../../App";
-import { useTheme } from "@material-ui/core/styles";
+import { useTheme, responsiveFontSizes } from "@material-ui/core/styles";
 
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
@@ -36,7 +36,7 @@ const defaultProps = {};
  * 1. There is no secondary color
  * 2. White on primary is not legible. Trying to set on bold + increase letter spacing to increase legibility.
  */
-const finsterTheme = {
+const finsterTheme1 = {
   palette: {
     secondary: {
       main: "#FA9140",
@@ -54,6 +54,145 @@ const finsterTheme = {
     }
   }
 };
+
+let finsterTheme = {
+  palette: {
+    primary: {
+      main: "#FA9140",
+      dark: "#F36D0C",
+      contrastText: "#FFFFFF"
+    },
+    secondary: {
+      main: "#FFFFFF",
+      contrastText: "#FA9140",
+      dark: "#f7f7f7"
+    },
+    text: {
+      main: "#000000",
+      white: "#FFFFFF"
+    },
+    background: {
+      default: "#F5F7FB"
+    },
+    common: {
+      white: "#FFFFFF",
+      black: "#000000",
+      grey: "#DBDBDB"
+    }
+  },
+  overrides: {
+    MuiInput: {
+      input: {
+        "&::placeholder": {
+          color: "#00000099"
+        }
+      }
+    },
+    MuiButton: {
+      root: {
+        height: 50,
+        letterSpacing: "0.078125rem",
+        fontWeight: 900,
+        lineHeight: "1rem",
+        fontSize: "0.875rem"
+      },
+      containedSecondary: {
+        // '&:hover': {
+        //   backgroundColor: 'transparent',
+        //   color: '#FFFFFF'
+        // }
+      }
+    },
+    MuiFormControlLabel: {
+      label: {
+        fontSize: "0.875rem"
+      }
+    },
+    MuiCheckbox: {
+      root: {
+        paddingRight: "1rem"
+      }
+    },
+    MuiTypography: {
+      root: {
+        color: "#000000"
+      }
+    }
+  },
+  typography: {
+    h1: {
+      fontSize: "6.125rem",
+      fontWeight: 900
+      // lineHeight: 1
+    },
+    h2: {
+      fontSize: "4.625rem",
+      fontWeight: 900
+      // lineHeight: '4rem'
+    },
+    h3: {
+      fontSize: "2.625rem",
+      // lineHeight: '3.4375rem',
+      fontWeight: 900
+    },
+    h4: {
+      fontSize: "1.9375rem",
+      // lineHeight: '2.625rem',
+      // lineHeight: 1,
+      fontWeight: 500
+    },
+    h5: {
+      fontSize: "1.75rem",
+      fontWeight: 700
+      // lineHeight: '2.5625rem'
+    },
+    h6: {
+      fontSize: "1.375rem",
+      fontWeight: 400
+    },
+    body1: {
+      fontSize: "1.125rem",
+      fontWeight: 400
+    },
+    body2: {
+      fontSize: "0.875rem",
+      fontWeight: 400
+      // lineHeight: '1.75rem'
+    }
+  },
+  mixins: {
+    authLink: {
+      color: "#FA9140",
+      lineHeight: "1.1875rem",
+      fontSize: "0.875rem",
+      fontWeight: 900,
+      textDecoration: "none"
+    },
+    center: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center"
+    },
+
+    centerVerticalAbsolute: {
+      top: "50%",
+      transform: "translateY(-50%)"
+    }
+  }
+};
+
+/*
+finsterTheme = responsiveFontSizes(finsterTheme);
+
+finsterTheme.mixins.toolbar = {
+  minHeight: 100,
+  height: 100,
+  [theme.breakpoints.down("sm")]: {
+    minHeight: 65,
+    height: 65
+  }
+};
+*/
 
 /**
  * Displays the component
